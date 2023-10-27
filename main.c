8d2578b963091c6200d8f8fee1284f7770028e11
OmarMesqq <omarmsqt@gmail.com>
2023-10-26 22:40:12 -0300

    refactor: :truck: use optivector in C code

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include "custom_vector.h"
 
-// Declare the extern functions
-extern CustomVector* create_vector();
-extern void push_back(CustomVector*, int);
-extern int at(CustomVector*, size_t);
-extern void swap_elements(CustomVector*, size_t, size_t);
-extern void delete_vector(CustomVector*);
+extern OptiVector* create_vector();
+extern void push_back(OptiVector*, int);
+extern int at(OptiVector*, size_t);
+extern void swap_elements(OptiVector*, size_t, size_t);
+extern void delete_vector(OptiVector*);
 
 int main() {
-    CustomVector* vec = create_vector();
+    OptiVector* vec = create_vector();
     push_back(vec, 1);
     push_back(vec, 2);
     push_back(vec, 3);
@@ -21,4 +21,3 @@ int main() {
     delete_vector(vec);
     return 0;
 }
-
